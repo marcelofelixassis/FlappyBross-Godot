ef24aee1ba5f7cd8473ec6e1e7ee780c94d906cd
marcelofelixassis <marcelofelix.af@gmail.com>
2017-11-02 20:20:16 -0200

    spawn ground and fixed background

---
+++ scripts/ground.gd
@@ -0,0 +1,16 @@
+#script: ground
+extends StaticBody2D
+
+onready var bottom_right = get_node("bottom_right")
+onready var camera = utils.get_main_node().get_node("camera")
+
+
+func _ready():
+	set_process(true)
+	pass
+
+func _process(delta):
+	if camera == null: return
+	if bottom_right.get_global_pos().x <= camera.get_total_pos().x:
+		queue_free()
+	pass
